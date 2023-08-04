888c3db117e73284f8aaacde828eaa0877ebad70
ocho <ochoo11@yahoo.com>
2023-08-04 18:04:38 -0400

    created Paddle class and import to main

--- pong_game/main.py
+++ pong_game/main.py
@@ -1,8 +1,13 @@
 from turtle import Screen
+from paddle import Paddle
 
 screen = Screen()
 screen.setup(width=800, height=600)
-screen.bgcolor("Black")
+screen.bgcolor("black")
 screen.title("Pong")
+
+screen.tracer(0)
+Paddle()
+screen.update()
 
 screen.exitonclick()
---
+++ pong_game/paddle.py
@@ -0,0 +1,12 @@
+from turtle import Turtle
+
+
+class Paddle(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.penup()
+        self.color("white")
+        self.shape("square")
+        self.shapesize(stretch_wid=5, stretch_len=1)
+        self.goto(350, 0)
+        self.speed("fastest")
