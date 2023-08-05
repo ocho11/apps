5608158810ecd13812e3af95ba07e3826fa9170c
ocho <ochoo11@yahoo.com>
2023-08-05 10:21:04 -0400

    removed transform trace

--- pong_game/main.py
+++ pong_game/main.py
@@ -1,15 +1,15 @@
-import turtle
+import turtle as game
 from paddle import Paddle
 
-screen = turtle.Screen()
+screen = game.Screen()
 screen.setup(width=800, height=600)
 screen.bgcolor("black")
 screen.title("Pong")
 
 player_1 = Paddle()
 
-turtle.listen()
-turtle.onkey(fun=player_1.up, key="Up")
-turtle.onkey(fun=player_1.down, key="Down")
+game.listen()
+game.onkey(fun=player_1.up, key="Up")
+game.onkey(fun=player_1.down, key="Down")
 
 screen.exitonclick()
--- pong_game/paddle.py
+++ pong_game/paddle.py
@@ -1,20 +1,25 @@
+import turtle
 from turtle import Turtle
 
 
 class Paddle(Turtle):
     def __init__(self):
         super().__init__()
+        turtle.tracer(0)
         self.penup()
         self.color("white")
         self.shape("square")
         self.shapesize(stretch_wid=5, stretch_len=1)
         self.goto(350, 0)
+        turtle.update()
         self.speed("fastest")
 
     def up(self):
         if self.ycor() < 250:
             self.goto(self.xcor(), self.ycor() + 20)
+            turtle.update()
 
     def down(self):
         if self.ycor() > -230:
             self.goto(self.xcor(), self.ycor() - 20)
+            turtle.update()
