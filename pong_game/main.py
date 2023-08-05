585350926e5725e8435575eacb9ac94c15061bc0
ocho <ochoo11@yahoo.com>
2023-08-05 12:35:45 -0400

    created player2 and implement screen.update on main.py

--- pong_game/main.py
+++ pong_game/main.py
@@ -6,10 +6,17 @@ screen.setup(width=800, height=600)
 screen.bgcolor("black")
 screen.title("Pong")
 
-player_1 = Paddle()
+player_1_right = Paddle((350, 0))
+player_2_left = Paddle((-350, 0))
 
 game.listen()
-game.onkey(fun=player_1.up, key="Up")
-game.onkey(fun=player_1.down, key="Down")
+game.onkey(fun=player_1_right.up, key="Up")
+game.onkey(fun=player_2_left.up, key="w")
+game.onkey(fun=player_1_right.down, key="Down")
+game.onkey(fun=player_2_left.down, key="s")
+
+game_is_on = True
+while game_is_on:
+    screen.update()
 
 screen.exitonclick()
