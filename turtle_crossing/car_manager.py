8552feff264f409d3f48b9776fe4d5f85cf5f754
ocho <ochoo11@yahoo.com>
2023-08-05 21:17:05 -0400

    created player class, cars class and player can move above, cars automatically created and move from left edge of screen to right side of screen

--- turtle_crossing/car_manager.py
+++ turtle_crossing/car_manager.py
@@ -1,7 +1,25 @@
+from turtle import Turtle
+import random
+
 COLORS = ["red", "orange", "yellow", "green", "blue", "purple"]
 STARTING_MOVE_DISTANCE = 5
 MOVE_INCREMENT = 10
 
 
-class CarManager:
-    pass
+class CarManager():
+    def __init__(self):
+        self.all_cars = []
+
+    def create_cars(self):
+        random_chance = random.randint(1, 6)
+        if random_chance == 5:
+            new_car = Turtle("square")
+            new_car.penup()
+            new_car.shapesize(stretch_wid=1, stretch_len=2)
+            new_car.color(random.choice(COLORS))
+            random_y_cor = random.randint(-250, 250)
+            new_car.goto(300, random_y_cor)
+            self.all_cars.append(new_car)
+    def move(self):
+        for car in self.all_cars:
+            car.backward(MOVE_INCREMENT)
--- turtle_crossing/main.py
+++ turtle_crossing/main.py
@@ -8,7 +8,17 @@ screen = Screen()
 screen.setup(width=600, height=600)
 screen.tracer(0)
 
+player = Player()
+car_manager = CarManager()
+
+screen.listen()
+screen.onkey(fun=player.move, key="Up")
+
 game_is_on = True
 while game_is_on:
     time.sleep(0.1)
     screen.update()
+
+    car_manager.create_cars()
+    car_manager.move()
+
