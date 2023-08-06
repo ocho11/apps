728f75f5265124367b6d926d0c2f24028071147d
ocho <ochoo11@yahoo.com>
2023-08-06 19:34:06 -0400

    fix a bug

--- mail_merge/main.py
+++ mail_merge/main.py
@@ -1,15 +1,11 @@
+PLACEHOLDER = "[name]"
 with open("./Input/Letters/starting_letter.txt") as letter:
     content_of_invitation = letter.read()
-
-
-def save_mail(mail_name, content_invitation):
-    with open(f"./Output/ReadyToSend/{mail_name}.txt", mode="w") as invitation:
-        invitation.write(content_invitation)
-
 
 with open("./Input/Names/invited_names.txt") as names:
     invited_names = names.read()
     names_list = invited_names.splitlines()
     for name in names_list:
-        content_of_invitation.replace("[name]", name)
-        save_mail(name, content_of_invitation)
+        new_content = content_of_invitation.replace(PLACEHOLDER, name)
+        with open(f"./Output/ReadyToSend/letter_for_{name}.txt", mode="w") as invitation:
+            invitation.write(new_content)
