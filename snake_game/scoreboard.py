21c4ba6ac5c7b1d353310c0e9f5e0f25de96b1e5
ocho <ochoo11@yahoo.com>
2023-08-06 15:53:08 -0400

    record high score to data.txt file and when app start get the high score from data.txt

--- snake_game/scoreboard.py
+++ snake_game/scoreboard.py
@@ -2,13 +2,19 @@ from turtle import Turtle
 
 ALIGNMENT = "center"
 FONT = ('Arial', 24, 'normal')
+DATA = "data.txt"
+
+
+def get_high_score_form_data():
+    with open(DATA) as file:
+        return file.read()
 
 
 class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
-        self.high_score = 0
+        self.high_score = int(get_high_score_form_data())
         self.color("White")
         self.penup()
         self.goto(10, 270)
@@ -26,5 +32,7 @@ class Scoreboard(Turtle):
     def reset(self):
         if self.score > self.high_score:
             self.high_score = self.score
+            with open(DATA, mode="w") as file:
+                file.write(str(self.high_score))
         self.score = 0
         self.update_score()
