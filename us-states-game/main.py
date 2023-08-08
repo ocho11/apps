2b809aeaf72684367bc3f073a3605919ff51644e
ocho <ochoo11@yahoo.com>
2023-08-07 21:08:14 -0400

    fix bugs

--- us-states-game/main.py
+++ us-states-game/main.py
@@ -1,9 +1,23 @@
 import turtle
+import pandas
 
 screen = turtle.Screen()
 screen.title("U.S. States Game")
+screen.setup(width=725, height=491)
 image = "blank_states_img.gif"
 screen.addshape(image)
 turtle.shape(image)
+
+answer_state = screen.textinput(title="Guess the State", prompt="What's another state's name?")
+data = pandas.read_csv("50_states.csv")
+all_states = data.state.to_list()
+
+if answer_state in all_states:
+    state = turtle.Turtle()
+    state.hideturtle()
+    state.penup()
+    state_data = data[data.state == answer_state]
+    state.goto(int(state_data.x), int(state_data.y))
+    state.write(state_data.state.item())
 
 screen.exitonclick()
