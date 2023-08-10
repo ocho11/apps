96f1f83aa991a2f2e723dd2d0930ea13f6d39f82
ocho <ochoo11@yahoo.com>
2023-08-09 21:06:58 -0400

    when click the start button, timer will be count down depend on work, short break, long break. If timer finish next phase will be start. One work time complete there will added check green mark

--- pomodoro/main.py
+++ pomodoro/main.py
@@ -9,18 +9,42 @@ FONT_NAME = "Courier"
 WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
+reps = 0
 
-
+# Timer
 def start_timer():
-    count_down(5 * 60)
+    global reps
+    reps += 1
+    work_sec = WORK_MIN * 60
+    short_break_sec = SHORT_BREAK_MIN * 60
+    long_break_sec = LONG_BREAK_MIN * 60
+
+    if reps % 8 == 0:
+        title_label.config(text="Break", fg=RED)
+        count_down(long_break_sec)
+    elif reps % 2 == 0:
+        title_label.config(text="Break", fg=PINK)
+        count_down(short_break_sec)
+    else:
+        title_label.config(text="Work", fg=GREEN)
+        count_down(work_sec)
 
 
 def count_down(count):
     count_minute = math.floor(count / 60)
     count_second = count % 60
+    if count_second < 10:
+        count_second = f"0{count_second}"
+
     canvas.itemconfig(timer_text, text=f"{count_minute}:{count_second}")
     if count > 0:
         window.after(1000, count_down, count - 1)
+    else:
+        start_timer()
+        checks = ""
+        if reps % 2 == 0:
+            checks += "✔︎"
+            check_marks.config(text=checks)
 
 
 # UI SETUP
@@ -43,7 +67,7 @@ start_button.grid(row=2, column=0)
 reset_button = Button(text="Reset", highlightthickness=0)
 reset_button.grid(row=2, column=2)
 
-check_marks = Label(text="✔︎", fg=GREEN, bg=YELLOW)
+check_marks = Label(fg=GREEN, bg=YELLOW)
 check_marks.grid(row=3, column=1)
 
 window.mainloop()
