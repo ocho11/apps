430f07f394de66c49511ec7665268794de4ab1e1
ocho <ochoo11@yahoo.com>
2023-08-09 20:02:56 -0400

    create count down logic and implementation when click the start button

--- pomodoro/main.py
+++ pomodoro/main.py
@@ -1,4 +1,5 @@
 from tkinter import *
+import math
 
 PINK = "#e2979c"
 RED = "#e7305b"
@@ -8,6 +9,19 @@ FONT_NAME = "Courier"
 WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
+
+
+def start_timer():
+    count_down(5 * 60)
+
+
+def count_down(count):
+    count_minute = math.floor(count / 60)
+    count_second = count % 60
+    canvas.itemconfig(timer_text, text=f"{count_minute}:{count_second}")
+    if count > 0:
+        window.after(1000, count_down, count - 1)
+
 
 # UI SETUP
 window = Tk()
@@ -20,10 +34,10 @@ title_label.grid(row=0, column=1)
 canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 tomato_img = PhotoImage(file="tomato.png")
 canvas.create_image(100, 112, image=tomato_img)
-canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
+timer_text = canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
 canvas.grid(row=1, column=1)
 
-start_button = Button(text="Start", highlightthickness=0)
+start_button = Button(text="Start", highlightthickness=0, command=start_timer)
 start_button.grid(row=2, column=0)
 
 reset_button = Button(text="Reset", highlightthickness=0)
