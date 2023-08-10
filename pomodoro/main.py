da8b4159b09dfd342f832e029c788f76f2855ed3
ocho <ochoo11@yahoo.com>
2023-08-10 09:28:01 -0400

    when click the reset button, count will stop and reset, check marks disappeare, the title will change to Timer

--- pomodoro/main.py
+++ pomodoro/main.py
@@ -10,6 +10,16 @@ WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
 reps = 0
+timer = None
+
+# Reset
+def reset_timer():
+    window.after_cancel(timer)
+    canvas.itemconfig(timer_text, text="00:00")
+    title_label.config(text="Timer")
+    check_marks.config(text="")
+    global reps
+    reps = 0
 
 # Timer
 def start_timer():
@@ -38,7 +48,8 @@ def count_down(count):
 
     canvas.itemconfig(timer_text, text=f"{count_minute}:{count_second}")
     if count > 0:
-        window.after(1000, count_down, count - 1)
+        global timer
+        timer = window.after(1000, count_down, count - 1)
     else:
         start_timer()
         checks = ""
@@ -64,7 +75,7 @@ canvas.grid(row=1, column=1)
 start_button = Button(text="Start", highlightthickness=0, command=start_timer)
 start_button.grid(row=2, column=0)
 
-reset_button = Button(text="Reset", highlightthickness=0)
+reset_button = Button(text="Reset", highlightthickness=0, command=reset_timer)
 reset_button.grid(row=2, column=2)
 
 check_marks = Label(fg=GREEN, bg=YELLOW)
