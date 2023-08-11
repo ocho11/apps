ce830e3076470eff4eaa2ba81e8ce9b110e8fc0f
ocho <ochoo11@yahoo.com>
2023-08-10 21:59:35 -0400

    when enter except alphabet, message will be appear, and process will do again

--- NATO-alphabet/main.py
+++ NATO-alphabet/main.py
@@ -1,9 +1,19 @@
 import pandas
 
 data = pandas.read_csv("nato_phonetic_alphabet.csv")
-entered_word = input("Enter a word: ").upper()
-print(entered_word)
 
 alphabets_and_NATO = {row.letter: row.code for (index, row) in data.iterrows()}
-entered_letters_with_NATO = [alphabets_and_NATO[letter] for letter in entered_word]
-print(entered_letters_with_NATO)
+
+
+def generate_phonetic():
+    entered_word = input("Enter a word: ").upper()
+    try:
+        entered_letters_with_NATO = [alphabets_and_NATO[letter] for letter in entered_word]
+    except KeyError:
+        print("Sorry, only letters in the alphabet please")
+        generate_phonetic()
+    else:
+        print(entered_letters_with_NATO)
+
+
+generate_phonetic()
