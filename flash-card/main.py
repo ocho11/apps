8e853be80f122c5677e01da7e7bc1f36db872eb6
ocho <ochoo11@yahoo.com>
2023-08-12 17:37:07 -0400

    refactor and fix csv bug

--- flash-card/main.py
+++ flash-card/main.py
@@ -1,28 +1,26 @@
-BACKGROUND_COLOR = "#B1DDC6"
-
 from tkinter import *
+import random
+import pandas
+
+BACKGROUND_COLOR = "#B1DDC6"
 
 window = Tk()
 window.title("Flashy")
 window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
 
-canvas = Canvas(width=800, height=530, bg=BACKGROUND_COLOR)
+canvas = Canvas(width=800, height=530, bg=BACKGROUND_COLOR, highlightthickness=0)
 front_img_card = PhotoImage(file="./images/card_front.png")
 canvas.create_image(400, 263, image=front_img_card)
-canvas.grid(row=0, rowspan=4, column=0, columnspan=3)
-
-label_of_title = Label(text="Title", font=("Arial", 32), bg="White")
-label_of_title.grid(row=1, column=1)
-
-label_of_word = Label(text="Word", font=("Arial", 45, "bold"), bg="White")
-label_of_word.grid(row=2, column=1)
+canvas.create_text(400, 150, text="English", font=("Arial", 40, "italic"))
+word = canvas.create_text(400, 263, text="Word", font=("Arial", 60, "bold"))
+canvas.grid(row=0, column=0, columnspan=2)
 
 incorrect_img = PhotoImage(file="./images/wrong.png")
 incorrect_button = Button(image=incorrect_img, highlightthickness=0)
-incorrect_button.grid(row=4, column=0)
+incorrect_button.grid(row=1, column=0)
 
 correct_img = PhotoImage(file="./images/right.png")
 correct_button = Button(image=correct_img, highlightthickness=0)
-correct_button.grid(row=4, column=2)
+correct_button.grid(row=1, column=1)
 
 window.mainloop()
