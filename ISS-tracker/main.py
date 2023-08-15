8678e9746218aebb8d66b57b713aa47b81e60207
ocho <ochoo11@yahoo.com>
2023-08-14 22:41:00 -0400

    get sunrise time and sunset time from silicon valley using https://api.sunrise-sunset.org/json. Also takedatetime.now()

--- ISS-tracker/main.py
+++ ISS-tracker/main.py
@@ -1,5 +1,27 @@
 import requests
+from datetime import datetime
+
+MY_LAT = 37.334790
+MY_LONG = -121.888140
 
 response = requests.get(url="http://api.open-notify.org/iss-now.json")
 response.raise_for_status()
 data = response.json()
+
+iss_latitude = float(data["iss_position"]["latitude"])
+iss_longitude = float(data["iss_position"]["longitude"])
+
+parameters = {
+    "lat": MY_LAT,
+    "lng": MY_LONG,
+    "formatted": 0,
+}
+
+response = requests.get("https://api.sunrise-sunset.org/json", params=parameters)
+response.raise_for_status()
+data = response.json()
+
+sunrise = int(data["results"]["sunrise"].split("T")[1].split(":")[0])
+sunset = int(data["results"]["sunset"].split("T")[1].split(":")[0])
+
+time_now = datetime.now()
