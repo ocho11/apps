11017f28811710b22194e11367b71cd9f3aff5cc
ocho <ochoo11@yahoo.com>
2023-08-17 15:42:36 -0400

    question come from quiz_brain to the window

--- quizzler/quiz_brain.py
+++ quizzler/quiz_brain.py
@@ -16,8 +16,7 @@ class QuizBrain:
         self.current_question = self.question_list[self.question_number]
         self.question_number += 1
         current_question_unescaped = html.unescape(self.current_question.text)
-        user_answer = input(f"Q.{self.question_number}: {current_question_unescaped} (True/False): ")
-        self.check_answer(user_answer)
+        return current_question_unescaped
 
     def check_answer(self, user_answer):
         correct_answer = self.current_question.answer
--- quizzler/ui.py
+++ quizzler/ui.py
@@ -1,10 +1,11 @@
 from tkinter import *
+from quiz_brain import QuizBrain
 
 THEME_COLOR = "#375362"
 
 
 class QuizInterface:
-    def __init__(self):
+    def __init__(self, quiz_brain: QuizBrain):
         self.window = Tk()
         self.window.title("Quizzler")
         self.window.config(padx=20, pady=20, bg=THEME_COLOR)
@@ -12,10 +13,12 @@ class QuizInterface:
         self.score_label = Label(text="Score: 0", fg="white", bg=THEME_COLOR)
         self.score_label.grid(row=0, column=1)
 
+        self.quiz = quiz_brain
         self.canvas = Canvas(width=300, height=250)
-        self.canvas.create_text(
+        self.quiz_text = self.canvas.create_text(
             150,
             125,
+            width=280,
             text="question",
             fill=THEME_COLOR,
             font=("Arial", 20, "italic")
@@ -30,4 +33,10 @@ class QuizInterface:
         self.no_button = Button(image=no_img, highlightthickness=0)
         self.no_button.grid(row=2, column=1)
 
+        self.get_next_question()
+
         self.window.mainloop()
+
+    def get_next_question(self):
+        question_text = self.quiz.next_question()
+        self.canvas.itemconfig(self.quiz_text, text=question_text)
