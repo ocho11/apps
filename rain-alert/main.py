cc4f78fcb62f63b2f35512876c1bcdb837fa859a
ocho <ochoo11@yahoo.com>
2023-08-19 14:36:59 -0400

    if it's going to rain within 12 hours my phone recieves a sms via twilio api

--- rain-alert/main.py
+++ rain-alert/main.py
@@ -1,9 +1,15 @@
 import requests
 import os
+from twilio.rest import Client
 
 weather_city = os.environ.get("WEATHER_CITY")
 api_key = os.environ.get("WEATHER_API_KEY")
-TOMMOROW_end_point = "https://api.tomorrow.io/v4/weather/forecast"
+tomorrow_end_point = "https://api.tomorrow.io/v4/weather/forecast"
+account_sid = os.environ['TWILIO_ACCOUNT_SID']
+auth_token = os.environ['TWILIO_AUTH_TOKEN']
+from_sms_phone_number = os.environ.get("TWILIO_FROM_PHONE_NUMBER")
+to_sms_phone_number = os.environ.get("TWILIO_TO_PHONE_NUMBER")
+
 weather_parameters = {
     "location": weather_city,
     "apikey": api_key,
@@ -11,7 +17,7 @@ weather_parameters = {
 }
 
 headers = {"accept": "application/json"}
-response = requests.get(TOMMOROW_end_point, params=weather_parameters, headers=headers)
+response = requests.get(tomorrow_end_point, params=weather_parameters, headers=headers)
 response.raise_for_status()
 
 weather_data = response.json()
@@ -23,4 +29,12 @@ for weather_data_hour in weather_data_hourly[:12]:
         will_rain = True
 
 if will_rain:
-    print("Bring an umbrella.")
+    client = Client(account_sid, auth_token)
+    message = client.messages \
+        .create(
+        body="It's going to rain today. Remember to bring an ☔️☔.",
+        from_=from_sms_phone_number,
+        to=to_sms_phone_number
+    )
+
+    print(message.status)
