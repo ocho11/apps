9277abbdbb251a6083f96d1e0f83f59d2ad3ae34
ocho <ochoo11@yahoo.com>
2023-08-19 19:28:14 -0400

    if prices change 10 dollars take 3 top news and send me sms those news

--- stock-news/main.py
+++ stock-news/main.py
@@ -1,5 +1,7 @@
 import requests
 import os
+import json
+from twilio.rest import Client
 
 STOCK = "AAPL"
 COMPANY_NAME = "Apple Inc"
@@ -9,6 +11,10 @@ STOCK_ENDPOINT = "https://www.alphavantage.co/query"
 STOCK_API_KEY = os.environ.get("STOCK_API_KEY")
 NEWS_ENDPOINT = "https://newsapi.org/v2/everything"
 NEWS_API_KEY = os.environ.get("NEWS_API_KEY")
+ACCOUNT_SID = os.environ['TWILIO_ACCOUNT_SID']
+AUTH_TOKEN = os.environ['TWILIO_AUTH_TOKEN']
+SMS_FROM = os.environ.get("TWILIO_FROM_PHONE_NUMBER")
+SMS_TO = os.environ.get("TWILIO_TO_PHONE_NUMBER")
 
 stock_exchange_parameters = {
     "function": "TIME_SERIES_DAILY",
@@ -22,9 +28,9 @@ news_parameters = {
     "apiKey": NEWS_API_KEY,
 }
 
-response = requests.get(STOCK_ENDPOINT, stock_exchange_parameters)
-response.raise_for_status()
-stock_data = response.json()["Time Series (Daily)"]
+response_stock = requests.get(STOCK_ENDPOINT, stock_exchange_parameters)
+response_stock.raise_for_status()
+stock_data = response_stock.json()["Time Series (Daily)"]
 
 stock_data_list = [value for (key, value) in stock_data.items()]
 yesterday_closing_price = float(stock_data_list[0]["4. close"])
@@ -33,5 +39,13 @@ day_before_yesterday_closing_price = float(stock_data_list[1]["4. close"])
 if abs(yesterday_closing_price - day_before_yesterday_closing_price) >= TEN_DOLLAR:
     response_news = requests.get(NEWS_ENDPOINT, params=news_parameters)
     response_news.raise_for_status()
-    news_data = response_news.json()["articles"][:3]
-    print(news_data)
+    three_articles = response_news.json()["articles"][:3]
+
+    client = Client(ACCOUNT_SID, AUTH_TOKEN)
+    three_messages = [f"Headline: {article['title']}.\nBrief: {article['description']}" for article in three_articles]
+    for message in three_messages:
+        mess = client.messages.create(
+            body=message,
+            from_=SMS_FROM,
+            to=SMS_TO,
+        )
