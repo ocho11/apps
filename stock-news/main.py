ec79cd65f768e6a8ba3c79e6706c36b2992a82e2
ocho <ochoo11@yahoo.com>
2023-08-19 18:48:51 -0400

    the diffrence price is over 10 dollar, get 3 news from newsapi.org

--- stock-news/main.py
+++ stock-news/main.py
@@ -1,16 +1,25 @@
 import requests
+import os
 
 STOCK = "AAPL"
 COMPANY_NAME = "Apple Inc"
+TEN_DOLLAR = 10
 
 STOCK_ENDPOINT = "https://www.alphavantage.co/query"
+STOCK_API_KEY = os.environ.get("STOCK_API_KEY")
 NEWS_ENDPOINT = "https://newsapi.org/v2/everything"
+NEWS_API_KEY = os.environ.get("NEWS_API_KEY")
 
 stock_exchange_parameters = {
     "function": "TIME_SERIES_DAILY",
     "symbol": STOCK,
     "datatype": "json",
-    "apikey": "EK34XYQHLN48Q4EX",
+    "apikey": STOCK_API_KEY,
+}
+
+news_parameters = {
+    "q": COMPANY_NAME,
+    "apiKey": NEWS_API_KEY,
 }
 
 response = requests.get(STOCK_ENDPOINT, stock_exchange_parameters)
@@ -21,5 +30,8 @@ stock_data_list = [value for (key, value) in stock_data.items()]
 yesterday_closing_price = float(stock_data_list[0]["4. close"])
 day_before_yesterday_closing_price = float(stock_data_list[1]["4. close"])
 
-if abs(yesterday_closing_price - day_before_yesterday_closing_price) >= 10:
-    print("Get News")
+if abs(yesterday_closing_price - day_before_yesterday_closing_price) >= TEN_DOLLAR:
+    response_news = requests.get(NEWS_ENDPOINT, params=news_parameters)
+    response_news.raise_for_status()
+    news_data = response_news.json()["articles"][:3]
+    print(news_data)
