444ef7824d9a98f2cb990939fdc856fd5aa230cb
ocho <ochoo11@yahoo.com>
2023-08-20 20:08:54 -0400

    post request to create graph and refactoring

--- habit-tracker/main.py
+++ habit-tracker/main.py
@@ -2,13 +2,35 @@ import os
 import requests
 
 PIXELA_ENDPOINT = "https://pixe.la/v1/users"
+TOKEN = os.environ.get("PIXELA_TOKEN")
+USER_NAME = "python20230820"
 
 user_params = {
-    "token": os.environ.get("PIXELA_TOKEN"),
-    "username": "python20230820",
+    "token": TOKEN,
+    "username": USER_NAME,
     "agreeTermsOfService": "yes",
     "notMinor": "yes",
 }
 
-response = requests.post(url=PIXELA_ENDPOINT, json=user_params)
-print(response.text)
+not_created_user = False
+if not_created_user:
+    response_user = requests.post(url=PIXELA_ENDPOINT, json=user_params)
+    print(response_user.text)
+
+GRAPH_ENDPOINT = f"{PIXELA_ENDPOINT}/{USER_NAME}/graphs"
+
+graph_params = {
+    "id": "graph",
+    "name": "Running",
+    "unit": "Km",
+    "type": "float",
+    "color": "shibafu"
+}
+
+headers = {
+    "X-USER-TOKEN": TOKEN
+}
+
+response_graph = requests.post(url=GRAPH_ENDPOINT, json=graph_params, headers=headers)
+print(response_graph.text)
+
