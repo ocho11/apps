570984a234671bf3fb5b34df5b0d070a7e93443d
ocho <ochoo11@yahoo.com>
2023-08-20 20:20:57 -0400

    post request to record running distance to pixe.la and refactoring code

--- habit-tracker/main.py
+++ habit-tracker/main.py
@@ -4,6 +4,9 @@ import requests
 PIXELA_ENDPOINT = "https://pixe.la/v1/users"
 TOKEN = os.environ.get("PIXELA_TOKEN")
 USER_NAME = "python20230820"
+GRAPH_ENDPOINT = f"{PIXELA_ENDPOINT}/{USER_NAME}/graphs"
+GRAPH_ID = "graph"
+PIXEL_ENDPOINT = f"{GRAPH_ENDPOINT}/{GRAPH_ID}"
 
 user_params = {
     "token": TOKEN,
@@ -17,20 +20,28 @@ if not_created_user:
     response_user = requests.post(url=PIXELA_ENDPOINT, json=user_params)
     print(response_user.text)
 
-GRAPH_ENDPOINT = f"{PIXELA_ENDPOINT}/{USER_NAME}/graphs"
-
 graph_params = {
-    "id": "graph",
+    "id": GRAPH_ID,
     "name": "Running",
     "unit": "Km",
     "type": "float",
     "color": "shibafu"
 }
-
 headers = {
     "X-USER-TOKEN": TOKEN
 }
 
-response_graph = requests.post(url=GRAPH_ENDPOINT, json=graph_params, headers=headers)
-print(response_graph.text)
+not_created_graph = False
+if not_created_graph:
+    response_graph = requests.post(url=GRAPH_ENDPOINT, json=graph_params, headers=headers)
+    print(response_graph.text)
 
+pixel_params = {
+    "date": "20230820",
+    "quantity": "1.5",
+}
+
+not_created_pixel = False
+if not_created_pixel:
+    response_pixel = requests.post(url=PIXEL_ENDPOINT, json=pixel_params, headers=headers)
+    print(response_pixel.text)
